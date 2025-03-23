fbbd3138af2f8cd2547ce450674c498ae906f3a3
wikigr14 <251151@edu.p.lodz.pl>
2025-03-23 02:53:48 +0100

    Poprawa sterowania aby opierało się o KeyCode. Dodanie drugiego gracza.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb; // physics go brrr
 
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode up;
+    public KeyCode down;
+
     private Vector2 movementDirection; // where bro movin?
 
 
@@ -19,8 +24,15 @@ public class PlayerController : MonoBehaviour
     // every frame, never stop hustlin
     void Update()
     {
-        // input from the NPC behind the keyboard
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // What the sigma moment fr fr
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(right)) moveX += 1;
+        if (Input.GetKey(left)) moveX -= 1;     //Not bussin
+        if (Input.GetKey(up)) moveY += 1;
+        if (Input.GetKey(down)) moveY -= 1;
+
+        movementDirection = new Vector2(moveX, moveY).normalized; //What the sigma fr fr
     }
 
     // physics loop, we movin diff now
